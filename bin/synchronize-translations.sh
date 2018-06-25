7a7294b161efc15c2e40b7dbcf72fdc340bbf622
Bertrand Dunogier <bertrand.dunogier@ez.no>
2018-06-25 12:19:07 +0200

    Improved translation sync script

--- bin/synchronize-translations.sh
+++ bin/synchronize-translations.sh
@@ -1,44 +1,50 @@
 #!/usr/bin/env sh
-# This script should be used to synchronize ezplatform-i18n repository
-# Translations must be up-to-date in all packages
-echo 'Translation synchronization';
+# Synchronize translation sources from all packages to ezplatform-i18n
+echo '# Translation synchronization';
 
-echo '# Clean corresponding ezplatform-i18n folder';
-rm -rf ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui/*.xlf
-rm -rf ./vendor/ezsystems/ezplatform-i18n/ezpublish-kernel/*.xlf
-rm -rf ./vendor/ezsystems/ezplatform-i18n/repository-forms/*.xlf
 
 echo '# Mirror the translation files';
-cp ./vendor/ezsystems/ezplatform-admin-ui/src/bundle/Resources/translations/*.xliff ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui
-cp ./vendor/ezsystems/ezpublish-kernel/eZ/Bundle/EzPublishCoreBundle/Resources/translations/*.xlf ./vendor/ezsystems/ezplatform-i18n/ezpublish-kernel
-cp ./vendor/ezsystems/repository-forms/bundle/Resources/translations/*.xlf ./vendor/ezsystems/ezplatform-i18n/repository-forms
 
-echo '# Fixing .xlf extensions'
-rename -s '.xliff' '.xlf' vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui/*
+echo "ezsystems/ezplatform-admin-ui"
+rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui/*
+cp ./vendor/ezsystems/ezplatform-admin-ui/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-admin-ui
+
+echo "ezsystems/ezpublish-kernel"
+rm -f ./vendor/ezsystems/ezplatform-i18n/ezpublish-kernel/*
+cp ./vendor/ezsystems/ezpublish-kernel/eZ/Bundle/EzPublishCoreBundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezpublish-kernel
+
+echo "ezsystems/repository-forms"
+rm -f ./vendor/ezsystems/ezplatform-i18n/repository-forms/*
+cp ./vendor/ezsystems/repository-forms/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/repository-forms
 
 if [ -d "./vendor/ezsystems/date-based-publisher" ]; then
-  echo "Sync ezsystems/date-based-publisher"
-  rm ./vendor/ezsystems/ezplatform-i18n/date-based-publisher/*.xlf
-  cp ./vendor/ezsystems/date-based-publisher/bundle/Resources/translations/*.xlf ./vendor/ezsystems/ezplatform-i18n/date-based-publisher
+  echo "ezsystems/date-based-publisher"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/date-based-publisher/*
+  cp ./vendor/ezsystems/date-based-publisher/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/date-based-publisher
 fi
 
 if [ -d "./vendor/ezsystems/flex-workflow" ]; then
-  echo "Sync ezsystems/flex-workflow"
-  rm ./vendor/ezsystems/ezplatform-i18n/flex-workflow/*.xlf
-  cp ./vendor/ezsystems/flex-workflow/bundle/Resources/translations/*.xlf ./vendor/ezsystems/ezplatform-i18n/flex-workflow
+  echo "ezsystems/flex-workflow"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/flex-workflow/*
+  cp ./vendor/ezsystems/flex-workflow/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/flex-workflow
 fi
 
 if [ -d "./vendor/ezsystems/ezplatform-page-builder" ]; then
-  rm ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-builder/*.xlf
-  cp ./vendor/ezsystems/ezplatform-page-builder/src/bundle/Resources/translations/*.xlf ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-builder
+  echo "ezsystems/ezplatform-page-builder"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-builder/*
+  cp ./vendor/ezsystems/ezplatform-page-builder/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-builder
 fi
 
 if [ -d "./vendor/ezsystems/ezplatform-page-fieldtype" ]; then
-  rm ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-fieldtype/*.xlf
-  cp ./vendor/ezsystems/ezplatform-page-fieldtype/src/bundle/Resources/translations/*.xlf ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-fieltype
+  echo "ezsystems/ezplatform-page-fieldtype"
+  rm -f ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-fieldtype/*
+  cp ./vendor/ezsystems/ezplatform-page-fieldtype/src/bundle/Resources/translations/* ./vendor/ezsystems/ezplatform-i18n/ezplatform-page-fieldtype
 fi
 
+echo '# Fixing .xlf extensions'
+rename -s '.xliff' '.xlf' vendor/ezsystems/ezplatform-i18n/*/*
+
 echo '# Strip english locale suffix from filename';
-rename 's/\.en\./\./g' ./vendor/ezsystems/ezplatform-i18n/*/*
+rename -s '.en' '' ./vendor/ezsystems/ezplatform-i18n/*/*
 
 echo 'Translation synchronization done !';
